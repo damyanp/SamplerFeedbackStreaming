86fe85fecadc306eb7c1071b3bcb0c56e800c908
Damyan Pepper <damyanp@microsoft.com>
2021-09-27 17:58:04 -0700

    tweak

--- src/Tour.cpp
+++ src/Tour.cpp
@@ -20,14 +20,16 @@ XMMATRIX Tour::Update(std::vector<SceneObjects::BaseObject *> const &objects, fl
     index[2] = (index[1] + 1) % m_stops.size();
     index[3] = (index[2] + 1) % m_stops.size();
 
+    float at = std::min(std::max(0.0f, t / 0.6f - 0.2f), 1.0f);
 
     float st = (std::sin(t * XM_PI - XM_PIDIV2) + 1) / 2;
-    
+    float sat = (std::sin(at * XM_PI - XM_PIDIV2) + 1) / 2;
+
 
     auto p = XMVectorCatmullRom(m_stops[index[0]].Pos, m_stops[index[1]].Pos, m_stops[index[2]].Pos,
                                 m_stops[index[3]].Pos, st);
 
-    auto q = XMQuaternionSlerp(m_stops[index[1]].Quat, m_stops[index[2]].Quat, st);
+    auto q = XMQuaternionSlerp(m_stops[index[1]].Quat, m_stops[index[2]].Quat, sat);
 
     return XMMatrixMultiply(XMMatrixRotationQuaternion(q), XMMatrixTranslationFromVector(p));
 }
@@ -53,23 +55,25 @@ void Tour::UpdateRoute(std::vector<SceneObjects::BaseObject *> const &objects)
         return Planet{position, std::max({XMVectorGetX(scale), XMVectorGetY(scale), XMVectorGetZ(scale)})};
     });
 
-    // sort by distance from origin
-    std::sort(planets.begin(), planets.end(), [](auto &l, auto &r) {
-        return XMVectorGetX(XMVector3Length(l.Pos)) < XMVectorGetX(XMVector3Length(r.Pos));
-    });
-
     XMVECTOR lastPos = XMVectorZero();
     m_stops.clear();
     m_stops.reserve(planets.size());
 
-    std::transform(planets.begin(), planets.end(), back_inserter(m_stops), [&lastPos](Planet const &p) {
-        XMVECTOR toLastPos = XMVector3Normalize(lastPos - p.Pos);
+    while (!planets.empty())
+    {
+        // sort by distance from lastPos
+        std::sort(planets.begin(), planets.end(), [lastPos](auto &l, auto &r) {
+            return XMVectorGetX(XMVector3Length(l.Pos - lastPos)) > XMVectorGetX(XMVector3Length(r.Pos - lastPos));
+        });
+
+        Planet const &p = planets.back();
+
+        XMVECTOR toLastPos = XMVector3Normalize(p.Pos - lastPos);
         if (XMVector3Equal(toLastPos, XMVectorZero()))
         {
             toLastPos = XMVectorSet(1, 0, 0, 0);
         }
 
-        // toLastPos = XMVectorSet(1, 0, 0, 0);
         XMVECTOR newPos = p.Pos + (toLastPos * p.Scale * 3);
 
         XMMATRIX m = XMMatrixLookAtLH(newPos, p.Pos, XMVectorSet(0, 1, 0, 0));
@@ -78,8 +82,9 @@ void Tour::UpdateRoute(std::vector<SceneObjects::BaseObject *> const &objects)
         XMMatrixDecompose(&scale, &rotQuat, &position, m);
 
         lastPos = newPos;
-        return Stop{position, rotQuat};
-    });
+        m_stops.push_back(Stop{position, rotQuat});
+        planets.pop_back();
+    }
 
     m_mu = 0;
 }
